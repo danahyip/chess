0e244dccc02f8daaface48a6253804d6de31e0fc
hemantsharma98 <sharmahemantoh@gmail.com>
2021-04-30 23:59:51 +0530

    Test File Updated

--- src/test/java/game/BoardTest.java
+++ src/test/java/game/BoardTest.java
@@ -6,8 +6,10 @@
 
 package game;
 
-import org.junit.Test;
+import java.io.IOException;
+import java.lang.reflect.*;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  *
@@ -22,8 +24,48 @@ public class BoardTest {
      * Test of getArrayChessBoard method, of class Board.
      */
     @org.junit.Test
-    public void testGetArrayChessBoard() {
+    public void testChessMain() {
         Main main = new Main();
+        try{
+            main.newGame();
+        }catch(Exception e){
+            System.out.println("New game test exception.");
+        }
     }
     
+    @org.junit.Test
+    public void testChessBoardNewFrameFunction(){
+        try{
+            Class targetClass = Class.forName("game");
+            Board board = (Board) targetClass.newInstance();
+            Method method = targetClass.getDeclaredMethod("newFrame", Integer.class,Integer.class);
+            method.setAccessible(true);
+        }catch(Exception e){
+            System.out.println("Exception in test frame");
+        }
+    }
+    
+    @org.junit.Test
+    public void testChessBoardTimerFunction(){
+        try{
+            Class targetClass = Class.forName("game");
+            Board board = (Board) targetClass.newInstance();
+            Method method = targetClass.getDeclaredMethod("timer", null);
+            method.setAccessible(true);
+        }catch(Exception e){
+            System.out.println("Exception in test timer");
+        }    
+    }
+    
+    @org.junit.Test
+    public void testChessBoardListenerMovesFunction(){
+        try{
+            Class<?> targetClass = Class.forName("game.Board$BoardListener");
+            Constructor<?> constructor = targetClass.getDeclaredConstructor(Board.class);
+            Method method = targetClass.getDeclaredMethod("noOfMoves", null);
+            constructor.setAccessible(true);
+        }catch(Exception e){
+            System.out.println("Exception in test moves");
+        }
+    }
 }
