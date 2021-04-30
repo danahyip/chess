a9ec458be623eb9e03458d935b2da5c6a475cad3
hemantsharma98 <45316587+hemantsharma98@users.noreply.github.com>
2021-04-30 12:39:55 -0400

    Update Pawn.java

--- src/main/java/game/Pawn.java
+++ src/main/java/game/Pawn.java
@@ -20,6 +20,9 @@ public class Pawn extends Figure {
      * @param field to display pawn on a board.
      * Change icons for pawn_black and pawn_white.
      */
+    /*
+    \ref T4_2
+    */
     public Pawn(int x, int y, boolean black, Field field) {
         super(x, y, black, field);
         if (isBlack) {
